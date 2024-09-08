a39d7e1edd0c6263d8c96676e4c4386b7a00a64a
dominikjaro <dominikjaro789@gmail.com>
2024-09-08 10:50:36 +0100

    IAM roles

--- modules/enabler/iam_roles.tf
+++ modules/enabler/iam_roles.tf
@@ -7,7 +7,7 @@ resource "google_service_account_iam_member" "iac_automation_permissions" {
 resource "google_service_account_iam_member" "iac_automation_permissions_workload_identity" {
   service_account_id = google_service_account.iac_automation.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${google_service_account.iac_automation.email}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.org_workload_identity_pool.name}/attribute.repository/dominikjaro/terraform"
 }
 
 resource "google_service_account_iam_member" "iac_automation_permissions_service_account_token_creator" {
